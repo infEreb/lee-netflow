5454c277c482eaae5cd85fb69386f0477d9bda72
infEreb <savushkin.egor.00027@gmail.com>
2023-01-24 14:25:26 +0300

    rule system loggers added

--- internal/domain/system/system.go
+++ internal/domain/system/system.go
@@ -4,6 +4,7 @@ import (
 	"lee-netflow/internal/domain/rule/matcher"
 	"lee-netflow/internal/domain/rule/parser"
 	"lee-netflow/internal/domain/rule/validator"
+	"log"
 )
 
 type System interface {
@@ -11,6 +12,10 @@ type System interface {
 	Configure(config_path string) error
 	// Main logic of system work
 	Run() error
+	// Loggers
+	InfoLog() log.Logger
+	DebugLog() log.Logger
+	ErrorLog() log.Logger
 	// Returns info about system
 	GetInfo() string
 }
