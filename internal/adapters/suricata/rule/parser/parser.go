1f255f2901a4668190a15675480329ecbf1d96ba
infEreb <savushkin.egor.00027@gmail.com>
2023-01-26 21:44:28 +0300

    Parse logic changed

--- internal/adapters/suricata/rule/parser/parser.go
+++ internal/adapters/suricata/rule/parser/parser.go
@@ -8,6 +8,7 @@ import (
 	"lee-netflow/internal/adapters/suricata/rule/element/constant"
 	"lee-netflow/internal/adapters/suricata/rule/element/direction"
 	"lee-netflow/internal/adapters/suricata/rule/element/group"
+	"lee-netflow/internal/adapters/suricata/rule/element/keyword"
 	"lee-netflow/internal/adapters/suricata/rule/element/option"
 	"lee-netflow/internal/adapters/suricata/rule/element/port"
 	"lee-netflow/internal/adapters/suricata/rule/element/portrange"
@@ -15,6 +16,7 @@ import (
 	"lee-netflow/internal/domain/rule"
 	"lee-netflow/internal/domain/rule/element"
 	"lee-netflow/internal/domain/rule/parser"
+	"lee-netflow/internal/domain/rule/validator"
 	"regexp"
 	"strings"
 )
@@ -42,8 +44,8 @@ func New() *SuricataParser {
 
 
 
-func (sp *SuricataParser) Parse(rule_text string, rule_name string) (ans *parser.ParserAnswer, err error) {
-	s_rule := rule.New(rule_name)
+func (sp *SuricataParser) Parse(rule_text string, rule_name string, valid validator.Validator) (ans *parser.ParserAnswer, err error) {
+	s_rule := rule.New(rule_name, rule_text)
 
 	if strings.HasPrefix(rule_text, "#") {
 		s_rule.Disable()
@@ -74,13 +76,13 @@ func (sp *SuricataParser) Parse(rule_text string, rule_name string) (ans *parser
 	proto, _ := ParseProtocol(elements[2])
 	s_rule.AddElement(proto, constants.ProtocolType)
 	// parse src address
-	src_addr, err := ParseAddress(elements[3], constants.SrcAddressType)
+	src_addr, err := ParseAddress(elements[3], constants.SrcAddressType, valid.GetBaseValidator())
 	if err != nil {
 		return &parser.ParserAnswer{IsRule: true, Rule: nil}, fmt.Errorf("%s. Rule: %s", err.Error(), rule_str)
 	}
 	s_rule.AddElement(src_addr, constants.SrcAddressType)
 	// parse src port
-	src_port, err := ParsePort(elements[4], constants.SrcPortType)
+	src_port, err := ParsePort(elements[4], constants.SrcPortType, valid.GetBaseValidator())
 	if err != nil {
 		return &parser.ParserAnswer{IsRule: true, Rule: nil}, fmt.Errorf("%s. Rule: %s", err.Error(), rule_str)
 	}
@@ -89,13 +91,13 @@ func (sp *SuricataParser) Parse(rule_text string, rule_name string) (ans *parser
 	dir, err := ParseDirection(elements[5])
 	s_rule.AddElement(dir, constants.DirectionType)
 	// parse dst address
-	dst_addr, err := ParseAddress(elements[6], constants.DstAddressType)
+	dst_addr, err := ParseAddress(elements[6], constants.DstAddressType, valid.GetBaseValidator())
 	if err != nil {
 		return &parser.ParserAnswer{IsRule: true, Rule: nil}, fmt.Errorf("%s. Rule: %s", err.Error(), rule_str)
 	}
 	s_rule.AddElement(dst_addr, constants.DstAddressType)
 	// parse src port
-	dst_port, err := ParsePort(elements[7], constants.DstPortType)
+	dst_port, err := ParsePort(elements[7], constants.DstPortType, valid.GetBaseValidator())
 	if err != nil {
 		return &parser.ParserAnswer{IsRule: true, Rule: nil}, fmt.Errorf("%s. Rule: %s", err.Error(), rule_str)
 	}
@@ -130,9 +132,27 @@ func (sp *SuricataParser) Parse(rule_text string, rule_name string) (ans *parser
 }
 
 // return element of correct type
-func ParseGroupElements(elem_str string, elem_type element.ElementType) (element.Element, error) {
+func ParseElements(elem_str string, elem_type element.ElementType, valid *validator.BaseValidator) (element.Element, error) {
 	if constants.IsConstant(elem_str) {
-		return constant.New(elem_str, action.New("constant")), nil
+		el_temp := constant.New(elem_str, action.New("constant"))
+		elems, err := valid.GetValidByType(constants.ConstantType)
+		if err != nil {
+			return nil, err
+		}
+		for _, el := range elems {
+			cp_el := el.Clone()
+			if cp_el.Compare(el_temp) {
+				if constants.IsSrcType(elem_type) {
+					cp_el.SetSrcType()
+				}
+				if constants.IsDstType(elem_type) {
+					cp_el.SetDstType()
+				}
+				return cp_el, nil
+			}
+		}
+
+		return nil, fmt.Errorf("Cannot find element %s with %s type as a valid element", elem_str, elem_type.GetName())
 	}
 
 	if constants.IsIPv4(elem_str) {
@@ -144,39 +164,64 @@ func ParseGroupElements(elem_str string, elem_type element.ElementType) (element
 	}
 
 	if constants.IsPortRange(elem_str) {
-		pr := portrange.New(elem_str, [][2]element.Element{}, []element.Element{})
-		eof_elem := false
-		elem := ""
-		for _, ch := range elem_str {
-			// if our element is port range
-			if eof_elem && strings.Contains(elem, ":") {
-				min_max := strings.Split(elem, ":")
-				pr.AddRange([2]element.Element{
-					port.New(min_max[0], elem_type), // min_max[0] = first port of range (for 1024:4096 is 1024)
-					port.New(min_max[1], elem_type), // min_max[1] = second port
-				})
-				elem = ""
-				eof_elem = false
-				continue
-			}
-			// if our element is just port
-			if eof_elem && !strings.Contains(elem, ":") {
-				pr.AddPort(port.New(elem, elem_type))
-				elem = ""
-				eof_elem = false
-				continue
-			}
-
-			if ch == '[' || ch == ']' || ch == ' ' {
-				continue
-			}
-
-			if ch == ',' {
-				eof_elem = true
-			}
-
-			elem += string(ch)
+		pr := portrange.New(elem_str, [2]element.Element{})
+		min_max := strings.Split(elem_str, ":")
+		if min_max[0] == "" {
+			min_max[0] = fmt.Sprint(constants.MIN_PORT)
 		}
+		if min_max[1] == "" {
+			min_max[1] = fmt.Sprint(constants.MAX_PORT)
+		}
+		pr.SetRange([2]element.Element{
+			port.New(min_max[0], elem_type),
+			port.New(min_max[1], elem_type),
+		})
+		// eof_elem := false
+		// elem := ""
+		// l := len(elem_str)
+		// for i := 0; i <= l; i++ {
+		// 	ch := byte(' ')
+		// 	if i != l {
+		// 		ch = elem_str[i]
+		// 	}
+		// 	// if our element is port range
+		// 	if eof_elem && strings.Contains(elem, ":") {
+		// 		min_max := strings.Split(elem, ":")
+		// 		if min_max[0] == "" {
+		// 			min_max[0] = fmt.Sprint(constants.MIN_PORT)
+		// 		}
+		// 		if min_max[1] == "" {
+		// 			min_max[1] = fmt.Sprint(constants.MAX_PORT)
+		// 		}
+		// 		pr.AddRange([2]element.Element{
+		// 			port.New(min_max[0], elem_type), // min_max[0] = first port of range (for 1024:4096 is 1024)
+		// 			port.New(min_max[1], elem_type), // min_max[1] = second port
+		// 		})
+		// 		elem = ""
+		// 		eof_elem = false
+		// 		continue
+		// 	}
+		// 	// if our element is just port
+		// 	if eof_elem && !strings.Contains(elem, ":") {
+		// 		pr.AddPort(port.New(elem, elem_type))
+		// 		elem = ""
+		// 		eof_elem = false
+		// 		continue
+		// 	}
+			
+		// 	if ch == '[' || ch == ']' || ch == ' ' {
+		// 		continue
+		// 	}
+			
+		// 	if ch == ',' {
+		// 		eof_elem = true
+		// 	}
+		// 	if i == l-1 && !eof_elem {
+		// 		eof_elem = true
+		// 	}
+
+		// 	elem += string(ch)
+		// }
 		return pr, nil
 	}
 
@@ -228,7 +273,7 @@ func ParseGroupElements(elem_str string, elem_type element.ElementType) (element
 			}
 
 			if eof_elem {
-				p_elem, err := ParseGroupElements(elem, elem_type)
+				p_elem, err := ParseElements(elem, elem_type, valid)
 				if err != nil {
 					return action.New("NULL"), err
 				}
@@ -243,6 +288,10 @@ func ParseGroupElements(elem_str string, elem_type element.ElementType) (element
 		return grp, nil
 	}
 
+	if constants.IsKeyword(elem_str) {
+		return keyword.New(elem_str), nil
+	}
+
 	return action.New("NULL"), fmt.Errorf("Has doesnt matched any element: %s", elem_str)
 }
 
@@ -254,40 +303,49 @@ func ParseProtocol(proto_str string) (*protocol.Protocol, error) {
 	return protocol.New(proto_str), nil
 }
 
-func ParseAddress(addr_str string, addr_type element.ElementType) (element.Element, error) {
-	p_addr, err := ParseGroupElements(addr_str, addr_type)
+func ParseAddress(addr_str string, addr_type element.ElementType, valid *validator.BaseValidator) (element.Element, error) {
+	p_addr, err := ParseElements(addr_str, addr_type, valid)
 	if err != nil {
 		return nil, err
 	}
 	if !p_addr.GetType().Compare(constants.AddressType) &&
 		!p_addr.GetType().Compare(constants.ConstantType) &&
-		!p_addr.GetType().Compare(constants.GroupType) {
-		return nil, fmt.Errorf("Value %s is not an address, constant or group", addr_str)
+		!p_addr.GetType().Compare(constants.GroupType) &&
+		!p_addr.GetType().Compare(constants.KeywordType) {
+		return nil, fmt.Errorf("Value %s is not an address, constant, keyword or group", addr_str)
 	}
 	return p_addr, nil
 }
 
-func ParsePort(port_str string, port_type element.ElementType) (element.Element, error) {
-	p_port, err := ParseGroupElements(port_str, port_type)
+func ParsePort(port_str string, port_type element.ElementType, valid *validator.BaseValidator) (element.Element, error) {
+	p_port, err := ParseElements(port_str, port_type, valid)
 	if err != nil {
 		return nil, err
 	}
 	if !p_port.GetType().Compare(constants.PortType) &&
 		!p_port.GetType().Compare(constants.ConstantType) &&
 		!p_port.GetType().Compare(constants.GroupType) &&
-		!p_port.GetType().Compare(constants.PortRangeType) {
-		return nil, fmt.Errorf("Value %s is not a port, constant, group or port range", port_str)
+		!p_port.GetType().Compare(constants.PortRangeType) &&
+		!p_port.GetType().Compare(constants.KeywordType) {
+		return nil, fmt.Errorf("Value %s is not a port, constant, keyword, group or port range", port_str)
 	}
 	return p_port, err
 }
 
-func ParseConstant(const_str string) (*constant.Constant, error) {
-	return constant.New(const_str, action.New("empty")), nil
+func ParseConstant(const_str string, elem_type element.ElementType, valid *validator.BaseValidator) (element.Element, error) {
+	p_const, err := ParseElements(const_str, elem_type, valid)
+	if err != nil {
+		return nil, err
+	}
+	return p_const, nil
 }
 
-func ParseGroup(group_str string, group_elem_type element.ElementType) (grp *group.Group, err error) {
-	p_grp, err := ParseGroupElements(group_str, group_elem_type)
-	return p_grp.(*group.Group), nil
+func ParseGroup(group_str string, group_elem_type element.ElementType, valid *validator.BaseValidator) (grp element.Element, err error) {
+	grp, err = ParseElements(group_str, group_elem_type, valid)
+	if err != nil {
+		return nil, err
+	}
+	return grp, nil
 }
 
 func ParseDirection(dir_str string) (*direction.Direction, error) {
