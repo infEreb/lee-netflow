21044f5be5ee4f13a8e1b89baf1715178bb9eaac
infEreb <savushkin.egor.00027@gmail.com>
2023-01-27 01:55:04 +0300

    CaptureInfo struct added GetCaptureInfo to System infterface added

--- internal/domain/system/system.go
+++ internal/domain/system/system.go
@@ -6,9 +6,17 @@ import (
 	"lee-netflow/internal/domain/rule/validator"
 )
 
+type CaptureInfo struct {
+	Ifaces  []string `json:"ifaces"`
+	Snaplen int32      `json:"snaplen"`
+	Promisc bool     `json:"promisc"`
+	Timeout int      `json:"timeout"`
+}
+
 type System interface {
 	// Main logic of configure system
 	Configure(config_path string) error
+	GetCaptureInfo() *CaptureInfo
 	// Main logic of system work
 	Run() error
 	// Loggers
@@ -19,7 +27,7 @@ type System interface {
 	GetInfo() string
 }
 
-type RuleFormatSystem interface {
+type SystemRuleFormat interface {
 	System
 	GetParser() parser.Parser
 	GetValidator() validator.Validator
